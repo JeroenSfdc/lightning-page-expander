b4ab15ba4b774230aa635f4f922ab7f8d39d2bbb
JeroenSfdc <>
2020-12-14 13:44:09 +0000

    [ClickDeploy] SFDP-8849 Flexi-page expander feature

--- aura/RecordPageWithCollapsiblePaneTemplate/RecordPageWithCollapsiblePaneTemplateController.js
+++ aura/RecordPageWithCollapsiblePaneTemplate/RecordPageWithCollapsiblePaneTemplateController.js
@@ -1,8 +1,5 @@
 ({
     handleExpand: function(component, event, helper) {
-        const expanded = event.getParam("expanded");
-        //TODO: Remove console logging
-        console.log('RecordPageWithCollapsiblePaneTemplate.handleExpand. Expanded = ' + expanded);
-        component.set("v.isSidebarCollapsed", expanded);
+        component.set("v.isSidebarCollapsed", event.getParam("expanded"));
     }
 })
